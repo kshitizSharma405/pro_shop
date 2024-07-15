6cb71d446a8dbb45f93f77a4cc9e967bc1d39638
Kshitiz Sharma <105395234+Naughtdefender@users.noreply.github.com>
2024-07-15 15:37:25 +0530

    added custom bootstrap css styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/header";
+import Footer from "./components/footer";
+
 const App = () => {
-  return <h1>Welcome to ProShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
